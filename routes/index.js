b16446f083997ac55076dc24b6facbd861736a86
VLad2 <vlad2@slb.com>
2024-07-01 20:26:54 +0530

    New changes updated

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Capstone Project 1', message: 'This is a capstone project to show NodeJS deployment on Azure app service' });
 });
 
 module.exports = router;
